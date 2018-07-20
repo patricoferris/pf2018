17f743796f038ccfcbe15fe41d62edb30ebd55e0
patricoferris <patrickferris17@gmail.com>
2018-07-20 23:27:44 +0100

    First commit

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/patrickferris/Documents/Projects/pf2018/src/layouts/index.js"
+  import data from "/Users/patrickferris/Documents/Projects/pf2018/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
