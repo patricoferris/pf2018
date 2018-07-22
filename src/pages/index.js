7c52c41187e31957a5441871e503f66745af498f
patricoferris <patrickferris17@gmail.com>
2018-07-22 18:38:28 +0100

    Update to mobile-responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,10 @@ import GistEmbed from '../components/GistEmbed';
 
 const IndexPage = () => (
   <div className='main-grid'>
-    <div style={{
-      gridColumnStart: `1`,
-      gridColumnEnd: `1`,
-      height: `100%`
-    }}>
+    <div className='image-holder'>
       <div id='home-image'></div>
     </div>
-    <div style={{
-      gridColumnStart: `3`,
-      gridColumnEnd: `3`
-    }}>
+    <div className='text-holder'>
       <h2 style={{fontFamily: `Courier`}}>Greetings...</h2>
       <p>My name is Patrick Ferris and I am a nineteen year old programmer, blogger and all-round tech enthusiast. I am currently Editor in Chief of the Hacker's at Cambridge blog.</p>
       <p>In the blog section you can check out all of my recent ideas and interests including the Medium articles I've been reading recently</p>
