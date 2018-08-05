131c2715e0ef9405b241d52e123a9e036c1edfb0
patricoferris <patrickferris17@gmail.com>
2018-08-05 16:01:27 +0100

    Fix mixed content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,7 +9,7 @@ const Contact = () => (
       <img src='https://res.cloudinary.com/patricoferris/image/upload/c_scale,w_150/v1533479255/pf2018/logos/github-logo.png'/>
     </a>
     <a href='https://twitter.com/PatricoFerris'>
-      <img src='http://res.cloudinary.com/patricoferris/image/upload/c_scale,w_150/v1533479253/pf2018/logos/twitter-logo.png'/>
+      <img src='https://res.cloudinary.com/patricoferris/image/upload/c_scale,w_150/v1533479253/pf2018/logos/twitter-logo.png'/>
     </a>
     <a href='https://medium.com/@patrickferris17'>
       <img src='https://res.cloudinary.com/patricoferris/image/upload/c_scale,w_150/v1533479254/pf2018/logos/medium-logo.png'/>
